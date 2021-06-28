fa3fe0a67e4892fcac4624aead08f0c6a4c93a35
Sajeesh007 <70201622+Sajeesh007@users.noreply.github.com>
2021-06-28 17:26:33 +0530

    first

---
+++ index.js
@@ -0,0 +1,44 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("send-message", (message,id) => {
+		socket.to(id).emit("recieve-message", message)
+	});
+
+	socket.on("send-info", (mic,video,id) => {
+		socket.to(id).emit("recieve-info", mic,video)
+	});
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
